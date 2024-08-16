8e69f3f0906ca78b4e37e70315f59f4808148493
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-08-16 20:05:20 +0600

    50% complete

--- script.js
+++ script.js
@@ -19,4 +19,22 @@ for(i = 0; i < accordion.length; i++){
 
 function accOn(element){
     element.style.color = "#fd307a";
-}
+};
+
+
+const menuBtn = document.querySelectorAll(".menu-icon");
+const nav = document.getElementById("nav");
+const open = document.querySelectorAll(".open");
+const close = document.querySelectorAll(".close");
+
+open[0].addEventListener("click", () =>{
+    nav.style.left ="0";
+    open[0].style.display = "none";
+    close[0].style.display = "block";
+});
+
+close[0].addEventListener("click", () => {
+    nav.style.left = "-100%";
+    open[0].style.display = "block";
+    close[0].style.display = "none";
+});
